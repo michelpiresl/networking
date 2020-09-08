bf2b2ef2edd3b04add3edaf788126bb0d81f1fa5
Michel Pires Lourenço <michelpireslourenco@gmail.com>
2020-09-07 22:32:46 -0300

    Initial Commit

---
+++ Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Michel Pires Lourenço on 07/09/20.
+//  Copyright © 2020 Michel Pires Lourenço. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
